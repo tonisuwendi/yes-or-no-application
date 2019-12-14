cb437382060897e98de4fd5dca42e772d2f16a15
tonisuwendi <tonisuwen@gmail.com>
2019-12-14 21:17:26 +0700

    add menu drawer slider

--- app/src/main/java/com/example/cekricek/MainActivity.kt
+++ app/src/main/java/com/example/cekricek/MainActivity.kt
@@ -2,21 +2,31 @@ package com.example.cekricek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.cekricek.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
 
         val navCtrl = this.findNavController(R.id.navhost_fragment)
-        NavigationUI.setupActionBarWithNavController(this, navCtrl)
+        NavigationUI.setupActionBarWithNavController(this, navCtrl, drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navCtrl)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navCtrl = this.findNavController(R.id.navhost_fragment)
-        return navCtrl.navigateUp()
+        return NavigationUI.navigateUp(navCtrl, drawerLayout)
     }
 }
